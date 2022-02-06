12d7e804cbda3490bdc8d823aaf407a71ee03b9c
TheodoreKaltsas <nycbrothers2012@gmail.com>
2022-02-05 22:28:45 -0500

    Added fixed timestep delta time to game class

--- Game.cpp
+++ Game.cpp
@@ -6,17 +6,36 @@
 
 #include "Game.h"
 
+const float Game::PlayerSpeed = 100.f;
+const sf::Time Game::TimePerFrame = sf::seconds(1.f / 60.f);
+
+//I created an initalization list to initialze values before the consturctor is called
 Game::Game()
     : mWindow(sf::VideoMode(640, 480), "SFML Application")
     , mPlayer()
+    , mIsMovingUp(false)
+    , mIsMovingDown(false)
+    , mIsMovingRight(false)
+    , mIsMovingLeft(false)
 {
     mPlayer.setRadius(40.f);
     mPlayer.setPosition(100.f, 100.f);
     mPlayer.setFillColor(sf::Color::Cyan);
 }
 
-void Game::update()
+void Game::update(sf::Time elapsedTime)
 {
+    sf::Vector2f movement(0.f, 0.f);
+    if (mIsMovingUp)
+        movement.y -= PlayerSpeed;
+    if (mIsMovingDown)
+        movement.y += PlayerSpeed;
+    if (mIsMovingLeft)
+        movement.x -= PlayerSpeed;
+    if (mIsMovingRight)
+        movement.x += PlayerSpeed;
+
+    mPlayer.move(movement * elapsedTime.asSeconds());
 }
 
 void Game::render()
@@ -26,23 +45,57 @@ void Game::render()
     mWindow.display();
 }
 
-void Game::run() {
+void Game::run()
+{
+    sf::Clock clock;
+    sf::Time timeSinceLastUpdate = sf::Time::Zero;
     while (mWindow.isOpen())
     {
-        processEvents();
-        update();
+        sf::Time elapsedTime = clock.restart();
+        timeSinceLastUpdate += elapsedTime;
+        while (timeSinceLastUpdate > TimePerFrame)
+        {
+            timeSinceLastUpdate -= TimePerFrame;
+
+            processEvents();
+            update(TimePerFrame);
+        }
+
         render();
     }
+}
+
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
+{
+    if (key == sf::Keyboard::W)
+        mIsMovingUp = isPressed;
+    else if (key == sf::Keyboard::S)
+        mIsMovingDown = isPressed;
+    else if (key == sf::Keyboard::A)
+        mIsMovingLeft = isPressed;
+    else if (key == sf::Keyboard::D)
+        mIsMovingRight = isPressed;
 }
 
 void Game::processEvents()
 {
     sf::Event event;
-
     while (mWindow.pollEvent(event))
     {
-        if (event.type == sf::Event::Closed)
+        switch (event.type)
+        {
+        case sf::Event::KeyPressed:
+            handlePlayerInput(event.key.code, true);
+            break;
+
+        case sf::Event::KeyReleased:
+            handlePlayerInput(event.key.code, false);
+            break;
+
+        case sf::Event::Closed:
             mWindow.close();
+            break;
+        }
     }
 }
 
--- Game.h
+++ Game.h
@@ -8,12 +8,19 @@ public:
 
 private:
 	void processEvents();
-	void update();
+	void update(sf::Time elapsedTime);
 	void render();
+	void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
 
 private:
 	sf::RenderWindow mWindow;
 	sf::CircleShape mPlayer;
+
+	bool mIsMovingUp, mIsMovingDown, mIsMovingRight, mIsMovingLeft;
+		
+
+	static const float PlayerSpeed;
+	static const sf::Time TimePerFrame;
 };
 
 
